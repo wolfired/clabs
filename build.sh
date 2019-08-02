069b29fc3bfa2b9a9c40b990a89c4930b4e0dfb6
LinkWu <wjianl@qq.com>
2019-08-02 14:29:35 +0800

    测试代码

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-rm -rf built && mkdir built && cd built && cmake .. && make && cd .. && ./built/main
+if [ '-h' == "$1" ] || [ '--help' == "$1" ]; then
+    echo "Usage:"
+    echo "  clean=1 ./build.sh"
+    echo "      Clean"
+    echo "  run=1 ./build.sh"
+    echo "      Run"
+    exit 0
+fi
+
+if [ -n "$clean" ]; then rm -rf out; fi \
+&& mkdir -p out \
+&& cd out \
+&& cmake -DCMAKE_BUILD_TYPE=Debug .. \
+&& make \
+&& if [ -n "$run" ]; then ./main; fi
--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "clabs.h"
 
 int main(int argc, char **argv) {
     trace("This is a trace msg!\n");
+
+    char* jscode = "console.log(\"Hello QJS!!\")";
+    eval_source(argc, argv, jscode, strlen(jscode));
+
     return 0;
 }
